a88c6b79ba714cd59582ee73aaa809903238c606
Yoni Amilcar Cisneros <cisneros1632@gmail.com>
2025-09-20 00:36:45 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import AppRouter from './routes/AppRouter';
 
+function App() {
+  return (
+    <div className="App">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
